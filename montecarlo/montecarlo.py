c5bc5dc51d1ed9721671fdcd036161d6a3a0d25f
gbravoi <glbravo@uc.cl>
2021-02-02 00:37:21 -0800

    changes to action  probabilies (on 1st depth)

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,4 +1,5 @@
 import random
+import seoulai_gym.envs.checkers.utils as checkers_utils
 
 class MonteCarlo:
 
@@ -8,6 +9,9 @@ class MonteCarlo:
 		self.node_evaluator = lambda child, montecarlo: None
 
 	def make_choice(self):
+		"""
+		choose the best chilndres from root.
+		"""
 		best_children = []
 		most_visits = float('-inf')
 
@@ -32,39 +36,70 @@ class MonteCarlo:
 
 			probabilities_already_counted += probability
 
-	def simulate(self, expansion_count = 1):
+	def simulate(self, expansion_count = 1,max_depth=50):
+		"""
+		simulate N branches (N=expansion_count)
+		max_depth: max depth of the branches
+		In difference of MTCS, where you keep expanding more promisiong nodes in the tree,
+		we will expand always one level below root devel, simulating a random game. the result of all the random games will help
+		ro decide the probability to win performing that move.
+		"""
 		for i in range(expansion_count):
 			current_node = self.root_node
 
-			while current_node.expanded:
-				current_node = current_node.get_preferred_child(self.root_node)
 
-			self.expand(current_node)
+			# while current_node.expanded:#search the prefered child with best probabilities to win. if is not expanded, continue expanding it
+			# 	current_node = current_node.get_preferred_child(self.root_node)
+			# 	current_depth+=1
+			# 	print("depth: {}".format(current_depth))
 
-	def expand(self, node):
-		self.child_finder(node, self)
+			self.expand(current_node,max_depth)#expand de unexplored leaft
+			
+
+	def expand(self, node,max_depth):
+		"""
+		expand all child of the selected node
+		performing random simulations on each branch
+		until the have a win or lose value
+		"""
+		if node.children == []: #added since we will expand several times the same childs. Need to look for child only the first time
+			self.child_finder(node, self)
 
 		for child in node.children:
 			child_win_value = self.node_evaluator(child, self)
 
-			if child_win_value != None:
+			if child_win_value != None:#run intil child is a win/lose
 				child.update_win_value(child_win_value)
-
-			if not child.is_scorable():
-				self.random_rollout(child)
+			else: #else, continue expanding the random simulation
+				current_depth=1
+				# print("depth: {}".format(current_depth))
+				self.random_rollout(child,max_depth,current_depth)
 				child.children = []
+				#once it finished expanding (i.e sinulation in branch ended), save game points
+				game_score=2*child.win_value[child.parent.player_number]-sum(child.win_value.values())
+				child.game_points.append(game_score)
+
 
 		if len(node.children):
 			node.expanded = True
 
-	def random_rollout(self, node):
+	def random_rollout(self, node,max_depth,current_depth):
+		"""
+		continue expanding branch, in random nodes intil win value is returned
+		limit to a max_depth of the branch
+		"""
 		self.child_finder(node, self)
-		child = random.choice(node.children)
+		child = random.choice(node.children)#expand current branch to a random child
 		node.children = []
 		node.add_child(child)
 		child_win_value = self.node_evaluator(child, self)
 
-		if child_win_value != None:
+		if child_win_value != None or max_depth<current_depth:#if is a win/lose stop. Can also stop because reached simulation max depth
 			node.update_win_value(child_win_value)
 		else:
-			self.random_rollout(child)
+			current_depth+=1
+			# print("depth: {}".format(current_depth))
+			# print("next child")
+			# checkers_utils.print_board(child.state.board_list)
+			# print("\n\r")
+			self.random_rollout(child,max_depth,current_depth)#if not, continue expanding branch
--- montecarlo/node.py
+++ montecarlo/node.py
@@ -5,24 +5,37 @@ class Node:
 
 	def __init__(self, state,player_number=None,transition_action={}):
 		self.state = state
-		self.transition_action=transition_action #save information of the action from the parent to bring to current status
-		self.win_value = 0
+		self.transition_action=transition_action #save information of the action from the parent to bring to current status. {(from_row, from_col):(to_row, to_col)}
+		self.action_reward=0 #reward for certain actions like capturing a checker or becoming a king (action to move from parent to child)
+		self.win_value = {} #store rewards for both player when doing backpropagation of the simulted game
+		self.game_points=[]#store final points of that game. computed as rewanr_payer- reward_opponent
 		self.policy_value = None
 		self.visits = 0
 		self.parent = None
 		self.children = []
 		self.expanded = False
 		self.player_number = player_number
-		self.discovery_factor = 0.35
+		self.discovery_factor =35.0
 
 	def update_win_value(self, value):
-		self.win_value += value
+		"""
+		we pass final value for each player. 
+		(At the end we should extrat the corresponding reward for the root node
+		"""
+		#recover rewards below on the tree
+		if not isinstance(value,dict): #if not a dictionary (case when stop before winning/lossing)
+			value={}
+		if self.player_number not in value:#make sure player is in the dictionary
+			value[self.player_number]=0
+		
+		self.win_value=value
+
+		#add win value in a dictionary with player number
+		self.win_value[self.player_number]+=self.action_reward		
 		self.visits += 1
 
-		if self.parent:
-			#check if different color, because win from one color is loss for the other
-			win_multiplier = 1 if self.parent.player_number == self.player_number else -1
-			self.parent.update_win_value(value*win_multiplier)
+		if self.parent:#pass current rewards to the parent on the branch
+			self.parent.update_win_value(self.win_value)
 
 	def update_policy_value(self, value):
 		self.policy_value = value
@@ -51,10 +64,15 @@ class Node:
 		return random.choice(best_children)
 
 	def get_score(self, root_node):
+		"""
+		get score, higher if root_node will win
+		"""
 		discovery_operand = self.discovery_factor * (self.policy_value or 1) * sqrt(log(self.parent.visits) / (self.visits or 1))
 
-		win_multiplier = 1 if self.parent.player_number == root_node.player_number else -1
-		win_operand = win_multiplier * self.win_value / (self.visits or 1)
+		player_reward =  self.win_value[root_node.player_number]
+		sum_reward=sum(self.win_value.values())
+		winning_points=2*player_reward-sum_reward#player-oponentet reward
+		win_operand = winning_points / (self.visits or 1)#maye add factor here?
 
 		self.score = win_operand + discovery_operand
 
